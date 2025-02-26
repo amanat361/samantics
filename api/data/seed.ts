7e746bff5c5e24890381ed458d9da4ae4a482259
amanat361 <amanat361@gmail.com>
2025-02-25 21:04:22 -0800

    feat: remove obsolete log and seed files along with old target words list

--- api/data/seed.ts
+++
@@ -1,9 +0,0 @@
-import { seedEmbeddings } from "../embeddings";
-
-async function main() {
-  console.log("Starting seeding process...");
-  await seedEmbeddings();
-  console.log("Seeding complete!");
-}
-
-main().catch(console.error);
